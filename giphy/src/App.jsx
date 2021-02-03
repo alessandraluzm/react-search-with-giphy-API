ae4a85b89f511364dd7df11ea1fb0b41a6ff967a
alessandraluzm <aledskywalker@gmail.com>
2021-02-02 22:26:30 -0300

    Cria componente de Loader

--- giphy/src/App.jsx
+++ giphy/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Giphy from './components/Giphy'
+import "./App.css"
 
 const App = () => {
     return <div><Giphy/></div>
--- giphy/src/components/Giphy.jsx
+++ giphy/src/components/Giphy.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import Loader from './Loader'
+
 const Giphy = () => {
     const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true)
             // Utiliza axios para chamar a API e salva o que recebe em results
             const results = await axios("https://api.giphy.com/v1/gifs/trending", {
                 params: {
@@ -15,11 +19,31 @@ const Giphy = () => {
             })
             console.log(results)
             setData(results.data.data)
+
+            // Seta isLoading para false após os dados carregarem
+            setIsLoading(false)
         }
 
         fetchData()
     }, [])
-    return <div className="">Giphy</div>
+
+    const renderGifs = () => {
+        // se isLoading é true, ativamos o loader
+        if(isLoading){
+            return <Loader />
+        }
+        return data.map(
+            el => {
+                return (
+                    <div key={el.id} className="gif">
+                        <img src={el.images.fixed_height.url} alt=""/>
+                    </div>
+                )
+            }
+        )
+    }
+
+    return <div className="container gifs">{renderGifs()}</div>
 }
 
 export default Giphy

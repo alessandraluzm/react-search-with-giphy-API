3aa899a9e635af5d36035e34db85b4e7c53d8261
alessandraluzm <aledskywalker@gmail.com>
2021-02-02 21:16:10 -0300

    Cria componente separado para o Giphy

--- giphy/src/components/Giphy.jsx
+++ giphy/src/components/Giphy.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Giphy = () => {
-    return <div className="">GIPHY</div>
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // Utiliza axios para chamar a API e salva o que recebe em results
+            const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+                params: {
+                    // Passa um dos parâmetros existentes na documentação
+                    api_key: "BneCXuIwLtCqfd2zUlX6BKOMSq4fRgXb"
+                }
+            })
+            console.log(results)
+            setData(results.data.data)
+        }
+
+        fetchData()
+    }, [])
+    return <div className="">Giphy</div>
 }
 
 export default Giphy
